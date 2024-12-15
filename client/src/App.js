d457b1d06156e58ff633ee39937b801250dd7621
Harshil Ambani <harshilambani1@gmail.com>
2024-12-15 18:33:26 +0530

    Theme Integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import TodoForm from './TodoForm';
 
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useRoutes } from 'react-router-dom';
+import Router from './routes/Router';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import { baselightTheme } from "./theme/DefaultColors";
 
-const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/todos')
-      .then(response => setTodos(response.data))
-      .catch(error => console.error(error));
-  }, []);
-
-  const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
-  };
+function App() {
+  
+  const routing = useRoutes(Router);
+  const theme = baselightTheme;
 
   return (
-    <div>
-      <div className="container">
-        <h1>MERN Stack Todo App</h1>
-        <TodoForm onAdd={addTodo} />
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo._id}>{todo.task}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {routing}
+    </ThemeProvider>
   );
-};
-export default App;
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';  // Use the 'client' version from react-dom
+import { BrowserRouter as Router } from 'react-router-dom';  // Import BrowserRouter
+import App from './App';  // Import your App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Create root
+
+root.render(  // Use 'render' method of the root to render your app
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
